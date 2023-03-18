d7230c4ddd3d2b0c2948105e785a84d22feab9a8
Sebastian009w <ia3266595@gmail.com>
2023-03-18 16:00:05 -0500

    instalation firebase --ini

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+  import { initializeApp } from "firebase/app";
+  import { firebaseConfig } from "../firebase.config";
+
+  const app = initializeApp(firebaseConfig);
   import Button from "../components/Button.svelte";
-  import Title from '../components/Title.svelte'
+  import Title from "../components/Title.svelte";
+
   let nameButton = "Welcome";
 </script>
 
 <div class="flex justify-center items-center h-screen flex-col">
-  <Title nameApp="Svelte App" cssTitle="text-5xl font-bold text-white mb-10"/>
-  <Button {nameButton} buttonCss="bg-red-500 rounded-md w-32 p-2"/>
+  <Title nameApp="Svelte App" cssTitle="text-5xl font-bold text-white mb-10" />
+  <Button {nameButton} buttonCss="bg-red-500 rounded-md w-32 p-2" />
 </div>
 
 <style lang="postcss">
