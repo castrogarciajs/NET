cddc79b8c0d5bd62836de01200ed4990ad2e0f2f
Sebastian009w <ia3266595@gmail.com>
2023-03-19 20:43:18 -0500

    se documenta los servicios de firebase --ini

--- src/firestore.ts
+++ src/firestore.ts
@@ -13,35 +13,35 @@ import type { NOTE } from "./interfaces/notes";
 
 /**
  *
- * @param callback onSub
- * @returns note
+ * @param callback esta funcion es la encargada de vigilar algun minimo cambio
+ * @returns devolvemos la nota que se ha añadido
  */
 export const getNote = (callback: (querySnapshot: QuerySnapshot) => void) =>
   onSnapshot(collection(firestore, "notes"), callback);
 
 /**
  *
- * @param title title note
- * @param description description note
- * @param image image note
- * @returns note
+ * @param title titulo de la nota
+ * @param description description de la nota
+ * @param image imagen de la nota
+ * @returns se devuelve la nota
  */
 export const saveNotes = (title: string, description: string, image: string) =>
   addDoc(collection(firestore, "notes"), { title, description, image });
 
 /**
  *
- * @param id note id
- * @returns note
+ * @param id obtenemos el id de la nota
+ * @returns se devuelve la nota eliminada
  */
 export const deleteNote = (id: string) =>
   deleteDoc(doc(firestore, "notes", id));
 
 /**
  *
- * @param id id note
- * @param note mew note
- * @returns note
+ * @param id id obtiene el id de la nota
+ * @param note se construye una la nota a remplazar
+ * @returns se devuelve la nota actulizada
  */
 export const updateNote = (id: string, note: NOTE) =>
   updateDoc(doc(firestore, "notes", id), { ...note });
