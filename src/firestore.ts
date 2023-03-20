bf7d3e898fb45a3ca75eee2f02e08a251ac9ac34
Sebastian009w <ia3266595@gmail.com>
2023-03-19 20:29:08 -0500

    documentation --ini

--- src/firestore.ts
+++ src/firestore.ts
@@ -12,16 +12,15 @@ import {
 import type { NOTE } from "./interfaces/notes";
 
 /**
- * 
+ *
  * @param callback onSub
  * @returns note
  */
-export const getNote = (
-  callback: (querySnapshot: QuerySnapshot) => void
-) => onSnapshot(collection(firestore, "notes"), callback);
+export const getNote = (callback: (querySnapshot: QuerySnapshot) => void) =>
+  onSnapshot(collection(firestore, "notes"), callback);
 
 /**
- * 
+ *
  * @param title title note
  * @param description description note
  * @param image image note
@@ -30,8 +29,19 @@ export const getNote = (
 export const saveNotes = (title: string, description: string, image: string) =>
   addDoc(collection(firestore, "notes"), { title, description, image });
 
+/**
+ *
+ * @param id note id
+ * @returns note
+ */
 export const deleteNote = (id: string) =>
   deleteDoc(doc(firestore, "notes", id));
 
+/**
+ *
+ * @param id id note
+ * @param note mew note
+ * @returns note
+ */
 export const updateNote = (id: string, note: NOTE) =>
   updateDoc(doc(firestore, "notes", id), { ...note });
