421852a992638c785b41c0e3be891a3087eebf6f
Sebastian009w <ia3266595@gmail.com>
2023-03-19 20:11:31 -0500

    documentation --ini

--- src/storage.ts
+++ src/storage.ts
@@ -1,9 +1,19 @@
+/**@storage firebase */
 import { storage } from "./firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
+/**@initialStorage */
 const storageRef = ref(storage, "static");
 
+/**
+ * 
+ * @param file image
+ * @returns url of tha image
+ */
 export const uploadFile = async (file: File) => {
+  /**
+   * @directory and nameof  image
+   */
   const fileRef = ref(storageRef, file.name);
   const uploadTask = uploadBytesResumable(fileRef, file);
 
