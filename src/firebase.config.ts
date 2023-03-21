f915f6e3dc036842941121d00a8b70270c5c752f
Sebastian009w <ia3266595@gmail.com>
2023-03-20 23:35:54 -0500

    bug production --ini

---
+++ src/firebase.config.ts
@@ -0,0 +1,12 @@
+
+/**
+ * @credentiales
+ */
+export const firebaseConfig = {
+  apiKey: "AIzaSyCq0rOcwZZLu8WhVt1AG5lKY6eEnmUg9Qc",
+  authDomain: "net-novice.firebaseapp.com",
+  projectId: "net-novice",
+  storageBucket: "net-novice.appspot.com",
+  messagingSenderId: "122726171780",
+  appId: "1:122726171780:web:fee03aea992fb2d39e2ed1"
+};
